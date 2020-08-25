4259de634548c250055e1975cd9855b830a3cd37
Stojan Anastasov <stolea@gmail.com>
2020-08-25 20:15:16 +0200

    AppModule.kt also init RxJavaDebug

--- app/src/main/java/io/github/lordraydenmk/superheroesapp/App.kt
+++ app/src/main/java/io/github/lordraydenmk/superheroesapp/App.kt
@@ -1,13 +1,23 @@
 package io.github.lordraydenmk.superheroesapp
 
 import android.app.Application
+import android.content.Context
+import com.akaita.java.rxjava2debug.RxJava2Debug
 import timber.log.Timber
 
 class App : Application() {
+
+    val appModule by lazy { AppModule.create() }
 
     override fun onCreate() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        // Enable RxJava assembly stack collection, to make RxJava crash reports clear and unique
+        // Make sure this is called AFTER setting up any Crash reporting mechanism as Crashlytics
+        RxJava2Debug.enableRxJava2AssemblyTracking()
     }
 }
+
+fun Context.appModule(): AppModule = (applicationContext as App).appModule
---
+++ app/src/main/java/io/github/lordraydenmk/superheroesapp/AppModule.kt
@@ -0,0 +1,71 @@
+package io.github.lordraydenmk.superheroesapp
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import io.github.lordraydenmk.superheroesapp.common.md5
+import io.github.lordraydenmk.superheroesapp.superheroes.data.SuperheroesService
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+/**
+ * Contains dependencies with Singleton/App scope
+ */
+interface AppModule : SuperheroesService {
+
+    companion object {
+
+        private val authInterceptor = { chain: Interceptor.Chain ->
+            val ts = System.currentTimeMillis()
+
+            val hash =
+                "$ts${BuildConfig.MARVEL_PRIVATE_API_KEY}${BuildConfig.MARVEL_PUBLIC_API_KEY}".md5()
+            val request = chain.request()
+            val url = request.url
+                .newBuilder()
+                .addQueryParameter("ts", ts.toString())
+                .addQueryParameter("apikey", BuildConfig.MARVEL_PUBLIC_API_KEY)
+                .addQueryParameter("hash", hash)
+                .build()
+            val updated = request.newBuilder()
+                .url(url)
+                .build()
+
+            chain.proceed(updated)
+        }
+
+        private val userAgentInterceptor = { chain: Interceptor.Chain ->
+            chain.proceed(
+                chain.request().newBuilder()
+                    .addHeader("User-Agent", "Superheroes app/${BuildConfig.VERSION_CODE}")
+                    .build()
+            )
+        }
+
+        private val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(userAgentInterceptor)
+            .addInterceptor(HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BASIC) })
+            .build()
+
+        private val contentType = "application/json".toMediaType()
+        private val converter = Json {
+            ignoreUnknownKeys = true
+        }
+
+        private val retrofit = Retrofit.Builder()
+            .baseUrl("https://gateway.marvel.com/v1/public/")
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+            .addConverterFactory(converter.asConverterFactory(contentType))
+            .client(client)
+            .build()
+
+        private val superheroesService = retrofit.create(SuperheroesService::class.java)
+
+        fun create(superheroesService: SuperheroesService = this.superheroesService): AppModule =
+            object : AppModule, SuperheroesService by superheroesService {}
+    }
+}
