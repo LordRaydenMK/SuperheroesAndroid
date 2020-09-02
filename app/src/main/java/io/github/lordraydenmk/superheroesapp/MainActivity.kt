ede113eff743e5a1ba3d6f3a51ab4b180985bf7e
Stojan Anastasov <stolea@gmail.com>
2020-09-02 16:54:58 +0200

    Add Navigation Component

--- app/src/main/java/io/github/lordraydenmk/superheroesapp/MainActivity.kt
+++ app/src/main/java/io/github/lordraydenmk/superheroesapp/MainActivity.kt
@@ -2,19 +2,11 @@ package io.github.lordraydenmk.superheroesapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
-import io.github.lordraydenmk.superheroesapp.superheroes.SuperheroesFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                add(R.id.container, SuperheroesFragment())
-            }
-        }
     }
 }
