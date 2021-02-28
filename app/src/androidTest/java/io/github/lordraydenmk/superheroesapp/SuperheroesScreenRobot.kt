43a1dd45a4430dca16691520268b7448e8440436
Stojan Anastasov <stolea@gmail.com>
2021-02-28 10:52:57 +0100

    Introduce a screen robot

---
+++ app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/SuperheroesScreenRobot.kt
@@ -0,0 +1,42 @@
+package io.github.lordraydenmk.superheroesapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.CoreMatchers.not
+
+fun superheroesScreen(f: SuperheroesScreenRobot.() -> Unit) =
+    SuperheroesScreenRobot().also(f)
+
+class SuperheroesScreenRobot {
+
+    private val loadingViewId = R.id.progressSuperheroes
+    private val recyclerViewId = R.id.rvSuperheroes
+    private val copyrightViewId = R.id.copyrightLayout
+    private val errorViewId = R.id.tvError
+
+    fun assertLoadingDisplayed() {
+        onView(withId(loadingViewId))
+            .check(matches(isDisplayed()))
+    }
+
+    fun assertErrorDisplayed(errorText: String) {
+        onView(withId(errorViewId)).check(matches(isDisplayed()))
+        onView(withId(errorViewId)).check(matches(withText(errorText)))
+    }
+
+    fun assertLoadingHidden() {
+        onView(withId(loadingViewId)).check(matches(not(isDisplayed())))
+    }
+
+    fun assertContentHidden() {
+        onView(withId(recyclerViewId)).check(matches(not(isDisplayed())))
+        onView(withId(copyrightViewId)).check(matches(not(isDisplayed())))
+    }
+
+    fun assertErrorHidden() {
+        onView(withId(errorViewId)).check(matches(not(isDisplayed())))
+    }
+}
--- app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/SuperheroesScreenTest.kt
+++ app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/SuperheroesScreenTest.kt
@@ -2,12 +2,18 @@ package io.github.lordraydenmk.superheroesapp
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.github.lordraydenmk.superheroesapp.ScreenScenario.Companion.launchInContainer
 import io.github.lordraydenmk.superheroesapp.common.ErrorTextRes
 import io.github.lordraydenmk.superheroesapp.common.IdTextRes
-import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.*
+import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.Content
+import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.Loading
+import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.Problem
+import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.SuperheroViewEntity
+import io.github.lordraydenmk.superheroesapp.superheroes.superheroslist.SuperheroesScreen
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.hamcrest.CoreMatchers.not
 import org.junit.Test
@@ -21,11 +27,11 @@ class SuperheroesScreenTest {
         val scenario = launchInContainer { parent -> SuperheroesScreen(parent) }
         scenario.onView { view -> view.bind(Loading).subscribe() }
 
-        onView(withId(R.id.progressSuperheroes)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.rvSuperheroes)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.copyrightLayout)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.tvError)).check(matches(not(isDisplayed())))
+        superheroesScreen {
+            assertLoadingDisplayed()
+            assertContentHidden()
+            assertErrorHidden()
+        }
     }
 
     @Test
@@ -35,12 +41,13 @@ class SuperheroesScreenTest {
             view.bind(Problem(ErrorTextRes(R.string.error_recoverable_network))).subscribe()
         }
 
-        onView(withId(R.id.tvError)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvError)).check(matches(withText("We could not connect to our server. Please check your internet connection \n\nTap to retry!")))
-
-        onView(withId(R.id.progressSuperheroes)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.rvSuperheroes)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.copyrightLayout)).check(matches(not(isDisplayed())))
+        val errorText =
+            "We could not connect to our server. Please check your internet connection \n\nTap to retry!"
+        superheroesScreen {
+            assertErrorDisplayed(errorText)
+            assertContentHidden()
+            assertLoadingHidden()
+        }
     }
 
     @Test
@@ -50,12 +57,12 @@ class SuperheroesScreenTest {
             view.bind(Problem(IdTextRes(R.string.error_unrecoverable))).subscribe()
         }
 
-        onView(withId(R.id.tvError)).check(matches(isDisplayed()))
-        onView(withId(R.id.tvError)).check(matches(withText("Ooops… Something went wrong!")))
-
-        onView(withId(R.id.progressSuperheroes)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.rvSuperheroes)).check(matches(not(isDisplayed())))
-        onView(withId(R.id.copyrightLayout)).check(matches(not(isDisplayed())))
+        val errorText = "Ooops… Something went wrong!"
+        superheroesScreen {
+            assertErrorDisplayed(errorText)
+            assertContentHidden()
+            assertLoadingHidden()
+        }
     }
 
     @Test
