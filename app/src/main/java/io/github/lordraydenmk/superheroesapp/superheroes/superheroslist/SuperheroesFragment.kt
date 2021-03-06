ec219eb5851caa4bbfdaa572c338a4b46b3445de
Stojan Anastasov <stolea@gmail.com>
2021-03-06 15:35:33 +0100

    Flow in the UI

--- app/src/main/java/io/github/lordraydenmk/superheroesapp/superheroes/superheroslist/SuperheroesFragment.kt
+++ app/src/main/java/io/github/lordraydenmk/superheroesapp/superheroes/superheroslist/SuperheroesFragment.kt
@@ -5,18 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import io.github.lordraydenmk.superheroesapp.AppModule
 import io.github.lordraydenmk.superheroesapp.R
 import io.github.lordraydenmk.superheroesapp.appModule
 import io.github.lordraydenmk.superheroesapp.common.presentation.ViewModelAlgebra
 import io.github.lordraydenmk.superheroesapp.common.rx.EffectsObserver
-import io.github.lordraydenmk.superheroesapp.common.rx.autoDispose
-import io.github.lordraydenmk.superheroesapp.common.rx.evalOn
 import io.github.lordraydenmk.superheroesapp.superheroes.superherodetails.SuperheroDetailsFragment
-import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.rx2.asFlow
-import kotlinx.coroutines.rx2.asObservable
+import kotlinx.coroutines.rx2.await
 
 class SuperheroesFragment : Fragment(R.layout.superheroes_fragment) {
 
@@ -45,16 +46,13 @@ class SuperheroesFragment : Fragment(R.layout.superheroes_fragment) {
             ViewModelAlgebra<SuperheroesViewState, SuperheroesEffect> by viewModel {}
 
         with(module) {
-            val renderObservable =
-                viewState.switchMap {
-                    screen.bind(it).toObservable<Unit>()
-                        .evalOn(AndroidSchedulers.mainThread())
-                }
+            val renderFlow = viewState.asFlow()
+                .mapLatest { screen.bind(it).await() }
 
-            program(screen.actions.asFlow())
-                .asObservable()
-                .mergeWith(renderObservable)
-                .autoDispose(viewLifecycleOwner)
+            lifecycleScope.launchWhenStarted {
+                merge(program(screen.actions.asFlow()), renderFlow)
+                    .collect()
+            }
         }
     }
 }
