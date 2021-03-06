7f0871e56de439bad61e1ef52ef647d6f51f3094
Stojan Anastasov <stolea@gmail.com>
2021-03-06 15:35:33 +0100

    Flow in the UI

--- app/src/main/java/io/github/lordraydenmk/superheroesapp/superheroes/superheroslist/SuperheroesScreen.kt
+++ app/src/main/java/io/github/lordraydenmk/superheroesapp/superheroes/superheroslist/SuperheroesScreen.kt
@@ -4,12 +4,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
-import com.jakewharton.rxbinding3.view.clicks
 import io.github.lordraydenmk.superheroesapp.common.presentation.Screen
 import io.github.lordraydenmk.superheroesapp.common.setTextResource
 import io.github.lordraydenmk.superheroesapp.databinding.SuperheroesScreenBinding
 import io.reactivex.Completable
 import io.reactivex.Observable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.rx2.asFlow
+import kotlinx.coroutines.rx2.asObservable
+import kotlinx.coroutines.rx2.rxCompletable
+import kotlinx.coroutines.withContext
+import reactivecircus.flowbinding.android.view.clicks
 
 class SuperheroesScreen(container: ViewGroup) : Screen<SuperheroesAction, SuperheroesViewState> {
 
@@ -25,12 +33,16 @@ class SuperheroesScreen(container: ViewGroup) : Screen<SuperheroesAction, Superh
         }
     }
 
-    override val actions: Observable<SuperheroesAction> = Observable.merge(
-        binding.tvError.clicks().map { Refresh },
-        superheroesAdapter.actions.map { LoadDetails(it) }
-    )
+    override val actions: Observable<SuperheroesAction> = merge(
+        binding.tvError.clicks().flowOn(Dispatchers.Main).map { Refresh },
+        superheroesAdapter.actions.asFlow().map { LoadDetails(it) }
+    ).asObservable()
 
-    override fun bind(viewState: SuperheroesViewState): Completable = Completable.fromCallable {
+    override fun bind(viewState: SuperheroesViewState): Completable =
+        rxCompletable { bindX(viewState) }
+
+    @Suppress("RedundantSuspendModifier") // updating the UI is a side effect
+    private suspend fun bindX(viewState: SuperheroesViewState) = withContext(Dispatchers.Main) {
         binding.groupSuperheroesContent.isVisible = viewState is Content
         binding.progressSuperheroes.isVisible = viewState is Loading
         binding.tvError.isVisible = viewState is Problem
