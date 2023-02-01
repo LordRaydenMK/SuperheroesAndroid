df16c5d22088699e629ce6fe6b3e8dd1eb3cb203
Stojan Anastasov <stojan.anastasov@axelspringer.com>
2023-02-01 07:35:40 +0100

    Print hierarchy on  e2e test failure (#191)
    * Create e2e.sh
    
    * Update ci.yml

---
+++ e2e.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# run first command
+maestro test app/src/maestro/flows/flow.yml
+result=$?
+
+# run second command if first one fails
+if [ $result -ne 0 ]; then
+  maestro hierarchy
+fi
+
+# return the result of the first command as the result of the script
+exit $result
