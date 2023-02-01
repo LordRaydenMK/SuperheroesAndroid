412549f3718049e9318bb005e86d680077ea00e2
Stojan Anastasov <stojan.anastasov@axelspringer.com>
2023-02-01 13:48:53 +0100

    Print logcat in case of test failure

--- e2e.sh
+++ e2e.sh
@@ -6,7 +6,10 @@ result=$?
 
 # run second command if first one fails
 if [ $result -ne 0 ]; then
+  echo "Printing hierarchy"
   maestro hierarchy
+  echo "Printing logcat for app"
+  adb logcat | grep "io.github.lordraydenmk.superheroesapp"
 fi
 
 # return the result of the first command as the result of the script
