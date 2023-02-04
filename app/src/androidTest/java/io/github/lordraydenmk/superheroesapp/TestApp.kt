dcc7da19482f5d43658278eae1bcf53590b0161b
Stojan Anastasov <stojan.anastasov@axelspringer.com>
2023-02-04 20:03:07 +0100

    Clean up after maestro migration (#203)

--- app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/TestApp.kt
+++ app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/TestApp.kt
@@ -3,15 +3,16 @@ package io.github.lordraydenmk.superheroesapp
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import io.github.lordraydenmk.superheroesapp.utils.TestingModule
 import io.github.lordraydenmk.superheroesapp.utils.testModule
+import kotlinx.serialization.ExperimentalSerializationApi
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import timber.log.Timber
 
+@ExperimentalSerializationApi
 class TestApp : Application(), ModuleOwner, ImageLoaderFactory {
 
     // appModule is now mutable so it can be replaced in tests
-    override var appModule: TestingModule = testModule("https://localhost".toHttpUrl())
+    override var appModule: AppModule = testModule("https://localhost".toHttpUrl())
 
     override fun onCreate() {
         super.onCreate()
--- app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/utils/testModule.kt
+++ app/src/androidTest/java/io/github/lordraydenmk/superheroesapp/utils/testModule.kt
@@ -1,17 +1,11 @@
 package io.github.lordraydenmk.superheroesapp.utils
 
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import coil.ImageLoader
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import io.github.lordraydenmk.superheroesapp.FakeImageLoader
-import io.github.lordraydenmk.superheroesapp.TestApp
 import io.github.lordraydenmk.superheroesapp.superheroes.data.SuperheroesService
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.withTimeout
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.HttpUrl
 import okhttp3.MediaType.Companion.toMediaType
@@ -19,8 +13,8 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
-import timber.log.Timber
 
+@ExperimentalSerializationApi
 fun testModule(baseUrl: HttpUrl): TestingModule {
 
     val client = OkHttpClient.Builder()
@@ -45,34 +39,5 @@ fun testModule(baseUrl: HttpUrl): TestingModule {
 
     return object : TestingModule, ImageLoader by FakeImageLoader, SuperheroesService by service {
 
-        private val state = MutableStateFlow<Any>(Unit)
-
-        override val afterBind: (Any) -> Unit = {
-            state.value = it
-            Timber.d("afterBind state=$it")
-        }
-
-        override suspend fun <A : Any> awaitState(clazz: Class<A>, timeoutMs: Long) {
-            try {
-                withTimeout(timeoutMs) {
-                    state.mapNotNull { if (clazz.isAssignableFrom(it.javaClass)) clazz.cast(it) else null }
-                        .first()
-                }
-            } catch (e: TimeoutCancellationException) {
-                Timber.d("Expected state to be type: ${clazz::class.java} found value: ${state.value}")
-                throw e
-            }
-        }
     }
-}
-
-suspend inline fun <reified T : Any> TestingModule.awaitState(timeoutMs: Long = 3_000): Unit =
-    awaitState(T::class.java, timeoutMs)
-
-fun testModule(): TestingModule =
-    (getInstrumentation().targetContext.applicationContext as TestApp).appModule
-
-fun replaceAppModule(testModule: TestingModule) {
-    val testApp = getInstrumentation().targetContext.applicationContext as TestApp
-    testApp.appModule = testModule
 }
